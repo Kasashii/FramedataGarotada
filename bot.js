6e2003efc1279e30c14a14d907e8ebeee04a961e
Kasashii <ikonokasashi@gmail.com>
2021-03-19 10:38:26 -0300

    phoda

--- bot.js
+++ bot.js
@@ -94,24 +94,6 @@ const arraydeeplef = [
   const csk = ("**Link para o combosuki:** \nhttps://combosuki.com/")
   const inteligente = ("https://media.discordapp.net/attachments/772262752340934670/821546797163347988/Capturar24.PNG")
   const ikki = ("```MAINS DA GAROTADA!```\n> IKKISOAD\nhttps://cdn.discordapp.com/attachments/617094243231465485/821855148787367986/IkkiMains.jpg")
-  const fukuajlp = new Discord.MessageEmbed()
-	.setColor('#0099ff')
-	.setTitle('FUKUA JLP')
-	.setURL()
-	.setAuthor()
-	.setDescription()
-	.setThumbnail('https://images-ext-2.discordapp.net/external/-Et8gJ7931agcva22E6D92VEAb0JdzZNKEUuPifAX3I/https/wiki.gbl.gg/images/f/fb/SG_fuk_jlp_hb.png')
-	.addFields( 
-		{ name: 'Guard', value: 'High', inline: true },
-		{ name: 'Damage', value: '200', inline: true },
-		{ name: 'Properties', value: '-', inline: true },
-		{ name: 'Meter', value: '2.50%', inline: true },
-	)
-	.addField()
-	.setImage('https://images-ext-1.discordapp.net/external/DrdmNMXmbkcge0sJKP7XaCKhoRvBpr7zbr9W0f7vy0I/https/wiki.gbl.gg/images/1/17/SG_fuk_jlp_fd.png')
-	.setTimestamp()
-	.setFooter('- Deceptively large hitbox with fast recovery.');
-  
   if(comando === "aaaaa") {
     message.channel.send ({files: [`./audios/aaaaaaaaaa.mp3`]});
   }
@@ -340,9 +322,6 @@ const arraydeeplef = [
   }
   if(comando === "mains_ikki") {
     message.channel.send ({files: [`./images/IkkiMains.jpg`]});
-  }
-  if(comando === "fukua_jlp") {
-    message.channel.send (fukuajlp)
   }
 });
 client.login(BOT_TOKEN);
