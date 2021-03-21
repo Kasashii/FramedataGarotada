0c1d6285afae3aac3245014a8c7916b786c1ba9a
Kasashii <ikonokasashi@gmail.com>
2021-03-21 20:24:46 -0300

    gay

--- bot.js
+++ bot.js
@@ -412,9 +412,10 @@ const arraydeeplef = [
   if(comando === "fg") {
     message.channel.send (fg)
   }
+  const xisde = (`${getRandomInt}`)
   let user = message.mentions.users.first() || client.users.cache.get(args[0]);
   if(comando === "gay") {
-    message.channel.send (`${user.username} **é`, getRandomInt,`% homossexual!**`)
+    message.channel.send (`${user.username} **é ${getRandomInt}% gay... Podem zoar ele, garotada!**`)
   }
 });
 client.login(BOT_TOKEN);
